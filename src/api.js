7edd4327034347ec6f6329c2e49750c63717e520
hu-maeruf <179197304+hu-maeruf@users.noreply.github.com>
2026-08-03 11:00:41 +0800

    feat: increase repos fetched per request

--- src/api.js
+++ src/api.js
@@ -7,7 +7,7 @@ export async function fetchOrg(orgName) {
 
 export async function fetchRepos(orgName) {
   const response = await fetch(
-    `https://api.github.com/orgs/${orgName}/repos?per_page=30`,
+    `https://api.github.com/orgs/${orgName}/repos?per_page=100`,
   );
   checkResponse(response);
   const data = await response.json();
